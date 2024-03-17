5acde38bd155f5fa55d1a0d0ec6c5ff1825c289b
czx0727 <zxchai.2022@mitb.smu.edu.sg>
2024-03-17 19:22:34 +0800

    shiny_test

--- project/app.R
+++ project/app.R
@@ -3,44 +3,60 @@ pacman::p_load(shiny,tidyverse)
 exam <- read_csv("data/Exam_data.csv")
 
 
-# Define UI for application that draws a histogram
+library(shiny)
+library(ggplot2)
+
+# Define UI for application that includes tabs
 ui <- fluidPage(
   titlePanel("Pupils Examination Results Dashboard"),
-  sidebarLayout(
-    sidebarPanel(
-      selectInput(inputId = "variable",
-                  label = "Subject:",
-                  choices = c("English" = "ENGLISH",
-                              "Maths" = "MATHS",
-                              "Science" = "SCIENCE"),
-                  selected = "ENGLISH"),
-      sliderInput(inputId = "bins",
-                  label = "Number of Bins",
-                  min = 5,
-                  max = 20,
-                  value= 10)
-    ),
-    mainPanel(plotOutput("distPlot"))
+  
+  tabsetPanel(
+    tabPanel("Tab 1", "Content for Tab 1"),
+    tabPanel("Tab 2", "Content for Tab 2"),
+    tabPanel("Tab 3", "Content for Tab 3"),
     
+    # Tab for Pupils examination results
+    tabPanel("Pupils",
+             sidebarLayout(
+               sidebarPanel(
+                 selectInput(inputId = "variable",
+                             label = "Subject:",
+                             choices = c("English" = "ENGLISH",
+                                         "Maths" = "MATHS",
+                                         "Science" = "SCIENCE"),
+                             selected = "ENGLISH"),
+                 sliderInput(inputId = "bins",
+                             label = "Number of Bins",
+                             min = 5,
+                             max = 20,
+                             value= 10)
+               ),
+               mainPanel(plotOutput("distPlot"))
+             )
+    )
   )
 )
 
-
-# Define server logic required to draw a histogram
+# Define server logic for histogram
 server <- function(input, output) {
   output$distPlot <- renderPlot({
-    x <- unlist(exam[,input$variable])
-    ggplot(exam, aes(x)) + 
+    # Sample data for demonstration
+    set.seed(123)
+    exam <- data.frame(
+      ENGLISH = rnorm(100, mean = 70, sd = 10),
+      MATHS = rnorm(100, mean = 65, sd = 8),
+      SCIENCE = rnorm(100, mean = 75, sd = 12)
+    )
+    
+    # Histogram plot
+    ggplot(exam, aes(x = exam[[input$variable]])) +
       geom_histogram(bins = input$bins,
-                     color="black",  
-                     fill="light blue") 
+                     color = "black",  
+                     fill = "light blue") +
+      labs(x = input$variable, y = "Frequency") +
+      theme_minimal()
   }) 
-  
-  
-  
-} 
-
+}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
